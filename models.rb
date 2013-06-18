4fe7a16485d5edb405a631f9e292914b118ad054
Nicolas ZERMATI <nicoolas25@gmail.com>
2013-06-18 16:10:38 +0200

    models only

---
+++ models.rb
@@ -0,0 +1,31 @@
+require 'digest'
+
+Post = Struct.new :author, :avatar, :content
+
+class Avatar
+  STORE_DIR = './avatars'
+
+  attr_reader :filename
+
+  def initialize(file)
+    file_content   = file.read
+    file_extension = File.extname file.path
+    content_hash   = Digest::SHA2.hexdigest file_content
+    @filename = content_hash + file_extension
+    write!(file_content)
+  end
+
+  private
+
+    def write!(file_content)
+      filepath = File.join(STORE_DIR, filename)
+      File.open(filepath, 'w') do |file|
+        file.write(file_content)
+      end
+    end
+end
+
+# Ensure Avatar::STORE_DIR is present.
+unless File.directory?(Avatar::STORE_DIR)
+  Dir.mkdir(Avatar::STORE_DIR)
+end
